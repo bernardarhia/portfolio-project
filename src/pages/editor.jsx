49c37fb61a9a25d8bd93d1449dcb03d10d446241
bernardarhia <megamindcooperations@gmail.com>
2020-12-23 17:00:44 +0000

    removed api folder

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -1,18 +1,72 @@
-import React from 'react'
-import Header from '../components/containers/Header'
+import React, { useState } from "react";
 
-const editor = () => {
-    return (
-        <>
-        <Header />
-        <div className="editor">
-            <section className="template-board">
-                <div className="template-editor"></div>
-                <div className="editing-toolbar"></div>
-            </section>
-        </div>
-        </>
-    )
-}
+const Editor = () => {
+  const [element, setElement] = useState(null);
+  const [fontSize, setFontSize] = useState(null);
+  const [templateStyle, setTemplateStyle] = useState({
+    introText: {
+      fontSize: "",
+    },
+    paraText: {
+      fontSize: "",
+    },
+  });
+ 
+  const handleFontSize = (e) => {
+    if (element !== null) {
+   
+      if(element.localName === 'h1'){
+        element.style.fontSize = e.target.value + "px";
+        setFontSize(e.target.value);
+        setTemplateStyle({
+          ...templateStyle,
+          introText:{
+            fontSize:e.target.value
+          }
+        })
+      }
+      if(element.localName === 'p'){
+        element.style.fontSize = e.target.value + "px";
+        setFontSize(e.target.value);
+        setTemplateStyle({
+          ...templateStyle,
+          paraText:{
+            fontSize:e.target.value
+          }
+        })
+      }
+    //  if(element.)
+    }
+  };
+  return (
+    <>
+      <h1
+      contentEditable={true}
+      suppressContentEditableWarning={true}
+        onClick={(e) => {
+          setElement(e.target);
+          setFontSize(+getComputedStyle(e.target).fontSize.replace("px", ""));
+          
+        }}
+        style={{resize:'both', overflow:'hidden'}}
+      >
+      Hello world
+      </h1>
 
-export default editor
+      <p
+        onClick={(e) => {
+          setElement(e.target);
+          setFontSize(+getComputedStyle(e.target).fontSize.replace("px", ""));
+        }}
+      >
+          Edit me
+      </p>
+
+      {element !== null && (
+        <input type="number" onChange={handleFontSize} value={fontSize} />
+      )}
+    </>
+  );
+};
+
+export default Editor;
