47a53a1b5957488f6d8243ae3e05b4639c6114a7
bernardarhia <megamindcooperations@gmail.com>
2020-12-25 18:06:32 +0000

    added final screen

--- src/pages/Final.jsx
+++ src/pages/Final.jsx
@@ -1,10 +1,23 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import FormHeaderText from '../components/containers/FormHeaderText'
 import FormScreen from '../components/screens/FormScreen'
 
 const Final = () => {
     return (
         <FormScreen>
-            final page
+             <motion.form
+        className="form"
+        animate={{ x: 0, scale: 1 }}
+        initial={{ x: "-100vw", scale: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+      >
+        <FormHeaderText className="">
+          <h1>
+           Almost Done
+          </h1>
+        </FormHeaderText>
+      </motion.form>
         </FormScreen>
     )
 }
