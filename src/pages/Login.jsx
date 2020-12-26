6f2c97d3a4cfe7eb4f58e93dbf5cdac5a8b19334
bernardarhia <megamindcooperations@gmail.com>
2020-12-26 04:56:00 +0000

    added login ad signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 import Button from "../components/containers/Button";
 import FormHeaderText from "../components/containers/FormHeaderText";
 import Input from "../components/containers/Input.";
 import FormScreen from "../components/screens/FormScreen";
-import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import Loader from "../components/containers/Loader";
+import { AiOutlineEye } from "react-icons/ai";
 const Login = ({ path }) => {
+  const [isLoading, setIsloading] = useState(false)
   const currentPage = path.replace("/", "");
+
+  const handleSubmit = e=>{
+    e.preventDefault()
+  }
   return (
     <FormScreen pageType={currentPage}>
-      <motion.form
-        className="form"
-        animate={{ x: 0, scale: 1 }}
-        initial={{ x: "-100vw", scale: 0 }}
-        transition={{ duration: 0.5, ease: "easeOut" }}
+      <form
+        onSubmit={handleSubmit}
       >
         <FormHeaderText className="">
           <h1>
             Howdy, <span>😊</span>
           </h1>
         </FormHeaderText>
-      </motion.form>
+
+        <div className="email">
+          <Input className="input" type="text" placeholder="Email" />
+        </div>
+        <div className="password">
+        <Input className="input" type="password" placeholder="Password" />
+        <AiOutlineEye />
+        </div>
+        <div className="submit_btn">
+          <Button className="btn btn_small blue">Log in</Button>
+        </div>
+        <div className="recovery" style={{display:'flex', justifyContent:'flex-end'}}>
+          <Link to='/recovery'>Recover password</Link>
+        </div>
+      </form>
     </FormScreen>
   );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,24 +4,55 @@ import FormHeaderText from "../components/containers/FormHeaderText";
 import Input from "../components/containers/Input.";
 import FormScreen from "../components/screens/FormScreen";
 import { motion } from "framer-motion";
-
-
+import { Link } from "react-router-dom";
+import {AiOutlineEye} from 'react-icons/ai'
 const Signup = ({ path }) => {
   const currentPage = path.replace("/", "");
 
   return (
     <FormScreen pageType={currentPage}>
       <motion.form
-        className="form"
-        initial={{ x: "-100vw", scale: 0, opacity:0 }}
-        animate={{ x: 0, scale: 1, opacity:1 }}
+        initial={{ x: "-100vw", scale: 0, opacity: 0 }}
+        animate={{ x: 0, scale: 1, opacity: 1 }}
         transition={{ duration: 0.5, ease: "easeOut" }}
       >
         <FormHeaderText className="">
-          <h1>
-           Create Account
-          </h1>
+          <h1>Create Account</h1>
         </FormHeaderText>
+
+        <div
+          className="name"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            gap: "2rem",
+          }}
+        >
+          <Input className="input" placeholder="First Name" type="text" />
+          <Input className="input" placeholder="Last Name" type="text" />
+        </div>
+        <div className="email">
+          <Input className="input" type="text" placeholder="Email" />
+        </div>
+        <div className="password">
+          <Input className="input" type="password" placeholder="Password" />
+          <AiOutlineEye />
+        </div>
+        <div className="recovery" style={{display:'flex',justifyContent:'space-between'}}>
+          <div className="terms_checked">
+            <Input type="checkbox" className="input"/>
+          </div>
+          <p>
+            {" "}
+            <span style={{ display: "block" }}>
+              By signing up, you have read and agreed to our{" "}
+            </span>
+            <Link to="/terms_and_conditions">terms and conditions</Link>
+          </p>
+        </div>
+        <div className="submit_btn">
+          <Button className="btn btn_small blue">Sign up</Button>
+        </div>
       </motion.form>
     </FormScreen>
   );
