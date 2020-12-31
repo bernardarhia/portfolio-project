cb403f5addc853dc3f492a698c647a0ab190af8b
Yussif <djiceman.gh@gmail.com>
2020-12-31 13:09:58 +0000

    template 1 initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PrivateRoute from "./routes/PrivateRoute";
 
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
+import template_1 from "./templates/template_1/template_1";
 function App() {
   const {user} = useContext(userContext);
   return (
@@ -19,6 +20,7 @@ function App() {
         <PublicRoute exact path="/login" component={Login} />
         <PublicRoute exact path="/signup" component={Signup}/>
         <PublicRoute exact path="/final" component={Final} />
+        <PublicRoute exact path="/template1" component={template_1} />
         <PrivateRoute exact path="/dashboard" isAuthenticated={user.isAuth} component={Dashboard}  user={user}/>
         <PrivateRoute exact path="/account" isAuthenticated={user.isAuth} component={Account}  user={user}/>
       </Switch>
