b3d896522de3699399ceb6564a2f8b781c8afd17
johnson <kryzbone@gmail.com>
2021-01-11 18:48:24 +0000

    wip

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-const PrivateRoute = ({ component: Component, isAuthenticated = true, ...rest }) => {
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
   return (
     <Route
       render={(props) => {
